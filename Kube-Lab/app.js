f6b4916958495c8df664e59b556cb06ea85f3b37
Zuhra Khan <zuhrasalmankhan@gmail.com>
2025-12-02 11:41:59 +0500

    Updated

--- Kube-Lab/app.js
+++ Kube-Lab/app.js
@@ -6,7 +6,7 @@ const port = 8080;          // Matches the port required in Lab Task 4
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from Kubernetes!\n'); 
+  res.end('Hello v2!\n'); 
 });
 
 server.listen(port, hostname, () => {
--- dockerfile
+++ dockerfile
@@ -1,13 +1,11 @@
 # Use a lightweight Node.js image
 FROM node:18-alpine
 
-# Set the working directory inside the container
 WORKDIR /app
 
 # Copy source code to the container
-COPY root/app.js .
+COPY /Kube-Lab/app.js .
 
-# Expose the port defined in the app
 EXPOSE 8080
 
 # Command to run the application
